e9bccab9e788a287943d86906c67db75496b76dc
elenisproject <elenis.karanikola@gmail.com>
2020-08-21 14:17:47 +0300

    requirements.txt updated

--- node_js/node_js_mysql/database2.js
+++ node_js/node_js_mysql/database2.js
@@ -11,26 +11,18 @@ const pool = mysql.createPool({
 
 // query rows in the table
 
-function queryRow(userName) {
-    //let selectQuery = 'SELECT * FROM ?? WHERE ?? = ?';  
+function queryRow(userName) { 
     let selectQuery = 'SELECT month(publish_time),count(journal) FROM ?? WHERE ?? = ? GROUP BY month(publish_time) ORDER BY month(publish_time)';  
     let query = mysql.format(selectQuery,["general","journal", userName]);
-    // query = SELECT * FROM `todo` where `user` = 'shahid'
     pool.query(query,(err, data) => {
         if(err) {
             console.error(err);
             return;
         }
-        // rows fetch
-        //return (data);
         console.log(data);
     });
 }
 
-// timeout just to avoid firing query before connection happens
-
 setTimeout(() => {
-    // call the function
-    // select rows
     queryRow('PLoS One');
 },5000);
